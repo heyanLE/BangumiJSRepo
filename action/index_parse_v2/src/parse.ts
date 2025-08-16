d14e9113e2cf65146b3a697e66e47a7c4e8a5782
heyanle <eke_le@163.com>
2025-08-16 18:22:35 +0800

    init

--- action/index_parse_v2/src/parse.ts
+++ action/index_parse_v2/src/parse.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 
 
-const repoUrl = "https://github.com/heyanLE/BangumiJSRepo/blob/publish/repository/v2"
+const repoUrl = "https://github.com/heyanLE/BangumiJSRepo/blob/main/repository/v2"
 
 const folder = "../..";
 const repositoryFolder = "../../repository/";
